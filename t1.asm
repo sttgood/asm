e2fa008a8515956a0844504652f18ffa153a170d
stt <776599148@qq.com>
2022-02-27 11:49:36 +0800

    new

--- t1.asm
+++ t1.asm
@@ -1,22 +1,30 @@
 assume cs:code
+;将a段中的数据逆序放到d段中
+a segment 
+		dw		1,2,3,4,5,6,7,8,0AH,0BH,0CH,0DH,0EH,0FH,0FFH
+a ends
 
-
+b segment 
+		dw		0,0,0,0,0,0,0,0
+b ends
 
 code segment
 
+;设置数据寄存器
+		mov ax,a
+		mov ds,ax
+;设置栈寄存器
+		mov ax,b
+		mov ss,ax
+		mov sp,16
+;设置中间参数，数据偏移，循环次数
+		mov bx,0
+		mov cx,8
 
-
-		mov bx,0B800H
-		mov es,bx
+pushData:push ds:[bx]
+		add bx,2
+		loop pushData
 		
-		mov bx,160*10 + 40*2
-
-		mov word ptr es:[bx],5535H
-
-
-
-
-
 
 		mov ax,4C00H
 		int 21H
